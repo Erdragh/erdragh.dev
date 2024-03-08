2f75d2892f57d4d93eb8c4677e2d95e0d5a58c31
Erdragh <mail@erdragh.dev>
2024-03-08 17:54:34 +0100

    Add metadata to about me and projects page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,10 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Erdragh - About Me",
+    description: "A little information about me as a person on the internet."
+}
+
 export default async function AboutMe() {
     return (
         <>
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,3 +1,10 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Erdragh - Projects",
+    description: "A little showcase of some of the projects I'm proud of."
+}
+
 export default async function Projects() {
     return (
         <>
