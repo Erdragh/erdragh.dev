2136b1abdfa161369655bb5e2180f20df336618e
Erdragh <mail@erdragh.dev>
2026-07-31 19:25:30 +0200

    Update about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -10,12 +10,12 @@ export default async function AboutMe() {
     return (
         <>
             <h1>About Me</h1>
-            <p>I&apos;m a Computer Science student from Germany. I&apos;m almost done with my Bachelor&apos;s thesis, which is about visualizing medical data in the browser using a volume pathtracer in WebGL.</p>
+            <p>I&apos;m a Software Developer from Germany. I have a Bachelor&apos;s degree in Computer Science, my thesis being about visualizing medical data in the browser using a volume pathtracer in WebGL.</p>
             <p>You can view a demo of the software online: <Link href={"https://volxel.github.io/Volxel"}>Volxel DICOM Viewer Demo</Link></p>
             <p>
-                I&apos;ve taken to web development as a profession and have been working as a web developer since before I went to university.
+                I&apos;ve taken to software development as a profession and have been working as a software developer since before I went to university.
                 I love programming and have been doing it ever since I was 13 years old. Two specific topics have crystallized as subtopics I enjoy most.
-                One is web development, which should be apparent, and the other one is graphics programming.
+                One is web development, which is less exciting, and the other one is graphics programming, which while challenging is very interesting.
             </p>
         </>
     );
